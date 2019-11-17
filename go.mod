69c2aeec16d0bb9bf94d43e58f9b0608174faa6c
m00qek <m00qek@gmail.com>
2019-11-17 19:27:31 -0300

    Control swaynag

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module sway-battery-nagbar
 
 go 1.13
+
+require github.com/mitchellh/go-ps v0.0.0-20190716172923-621e5597135b
---
+++ swaynag.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"os"
+	"os/exec"
+	"strings"
+
+	ps "github.com/mitchellh/go-ps"
+)
+
+type MessageProcess struct {
+	PID     int
+	Display string
+}
+
+func show(message string, display string) (*MessageProcess, error) {
+	cmd := exec.Command("swaynag", "--message", message, "--output", display)
+
+	err := cmd.Start()
+	if err != nil {
+		return nil, err
+	}
+
+	go func() {
+		cmd.Wait()
+	}()
+
+	return &MessageProcess{PID: cmd.Process.Pid, Display: display}, nil
+}
+
+func isSwaynag(process ps.Process, err error) bool {
+	return err == nil &&
+		process != nil &&
+		strings.HasSuffix(process.Executable(), "swaynag")
+}
+
+func ShowMessage(message string, process MessageProcess) (*MessageProcess, error) {
+	if isSwaynag(ps.FindProcess(process.PID)) {
+		return &process, nil
+	}
+	return show(message, process.Display)
+}
+
+func CloseMessage(process MessageProcess) {
+	systemProcess, err := os.FindProcess(process.PID)
+	if err != nil {
+		return
+	}
+
+	defer systemProcess.Release()
+	systemProcess.Signal(os.Interrupt)
+}
