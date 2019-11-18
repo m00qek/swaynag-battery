059050dce009f8d9dd61a29ec4e719077d035cd3
m00qek <m00qek@gmail.com>
2019-11-18 11:09:56 -0300

    Get available displays

---
+++ swaymsg.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"encoding/json"
+	"os/exec"
+)
+
+type Display struct {
+	Name   string
+	Active bool
+}
+
+func run() string {
+	cmd := exec.Command("swaymsg", "-t", "get_outputs")
+	output, _ := cmd.Output()
+	return string(output)
+}
+
+func filterActive(displays []Display) []string {
+	var activeDisplays []string
+	for _, display := range displays {
+		if display.Active {
+			activeDisplays = append(activeDisplays, display.Name)
+		}
+	}
+	return activeDisplays
+}
+
+func ActiveDisplays() []string {
+	var displays []Display
+	jsonOutput := run()
+	json.Unmarshal([]byte(jsonOutput), &displays)
+	return filterActive(displays)
+}
